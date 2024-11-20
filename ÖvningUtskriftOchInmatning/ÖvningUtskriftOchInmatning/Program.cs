396447ef3b0f24876e5fbac551dee0a26a964241
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-20 14:24:40 +0100

    Create assignment folder.

---
+++ ÖvningUtskriftOchInmatning/ÖvningUtskriftOchInmatning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Instruktioner:
+ *      1. Läsa in användarens för och efternamn och spara detta i variabler
+ *      2. Hälsa användaren välkommen 
+ *      3. Fråga användaren om deras ålder i år och spara detta i en variabel med
+ *          lämplig datatyp
+ *      4. Räkna ut antalet dagar personen har levt utifrån den angivna åldern
+ *          (ålder multiplicerat med 365) och presentera detta.
+ * 
+ * Författare:
+ *      David Browning
+ *      david.a.browning@gmail.com
+ */
+
+namespace ÖvningUtskriftOchInmatning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
