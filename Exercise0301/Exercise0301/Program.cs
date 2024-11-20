958d7103078f96fa4226ac0126a7cf62ef0d6fd5
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-20 13:21:55 +0100

    Complete exercise 0301.

--- Exercise0301/Exercise0301/Program.cs
+++ Exercise0301/Exercise0301/Program.cs
@@ -10,6 +10,25 @@ namespace Exercise0301
     {
         static void Main(string[] args)
         {
+            // Variables
+            string userInput;
+            int integer1;
+            int integer2;
+            int sum;
+
+            // Ask user for two integers
+            Console.WriteLine("Please enter an integer:");
+            userInput = Console.ReadLine();
+            integer1 = Convert.ToInt32(userInput);
+            Console.WriteLine("Please enter another integer:");
+            userInput = Console.ReadLine();
+            integer2 = Convert.ToInt32(userInput);
+
+            // Calculate sum
+            sum = integer1 + integer2;
+
+            // Display result
+            Console.WriteLine(integer1 + " + " + integer2 + " = " + sum);
         }
     }
 }
