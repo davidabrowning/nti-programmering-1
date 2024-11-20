39fd7ea0ae942c9b61eebf13673e95d05d0af3bf
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-20 13:40:39 +0100

    Create exercise 3.3 folder.

---
+++ Exercise0303/Exercise0303/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * ASSIGNMENT INSTRUCTIONS
+ * Skapa ett program som omvandlar ett inmatat decimaltal till närmsta heltal.
+ * Exempel:
+ *      Ange ett decimaltal: 11,534
+ *      Decimaltalet 11,534 avrundas till heltalet 12
+ */
+
+namespace Exercise0303
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+
+            // Ask user for decimal
+
+            // Round decimal to nearest integer
+
+            // Display results
+        }
+    }
+}
