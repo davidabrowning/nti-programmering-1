eac21021e66e58ddf3fb9373b859e8ccb7544382
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-20 11:36:34 +0100

    Lägga till uppgift instruktioner och program beskrivning.

--- VariablerDotNet/VariablerDotNet/Program.cs
+++ VariablerDotNet/VariablerDotNet/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * UPPGIFT INSTRUKTIONER:
+ * Skapa ett program där du initierar tre variabler. En strängvariabel med
+ * ditt förnamn, en annan strängvariabel med ditt efternamn och en
+ * int-variabel med din ålder. Skriv sedan ut värdet av dessa tre
+ * variabler, så att de syns i konsolfönstret.
+ * 
+ * PROGRAM BESKRIVNING:
+ * 1. Initiera tre variabler: förnamn, efternamn, ålder
+ * 2. Skriv ut värden
+ */
+
 namespace VariablerDotNet
 {
     internal class Program
