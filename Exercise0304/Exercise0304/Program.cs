7d2059c93861a49b66936c77e5e4d654bd113034
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-20 13:52:37 +0100

    Create exercise 3.4.

---
+++ Exercise0304/Exercise0304/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * ASSIGNMENT DESCRIPTION
+ * Låt användaren mata in namnet på två personer. Programmet ska sedan skriva
+ * ut en kort berättelse om dessa två personer.
+ */
+
+namespace Exercise0304
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
