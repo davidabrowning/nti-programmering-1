126a4b1f2f4c516acba026bcaebbf9e73449702e
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-22 15:11:01 +0100

    Complete exercise 4.1.

---
+++ Exercise0401/Exercise0401/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * UPPGIFT
+ * Skapa ett program som ställer frågan "Är det fint väder?" Om användaren
+ * svarar "j" skriver programmet "Vi går på picknick!" Annars händer ingenting.
+ * 
+ * PSUEDOKOD
+ * Ställ frågan "Är det fint väder?"
+ * OM svaret är j SÅ
+ *  Skriv "Vi går på picknick!"
+ * SLUT OM
+ */
+
+namespace Exercise0401
+{
+    internal class Program
+    {
+        // Initiera konstanter
+        const string frågan = "Är det fint väder?";
+        const string förslag = "Vi går på picknick!";
+        const string jaSvar = "j";
+
+        static void Main(string[] args)
+        {
+            // Initiera variabler
+            string användarSvar = "";
+
+            // Ställ frågan
+            Console.WriteLine(frågan);
+
+            // OM svaret är j SÅ
+            //  Skriva ut förslaget
+            // SLUT OM
+            användarSvar = Console.ReadLine();
+            if (användarSvar == jaSvar)
+            {
+                Console.WriteLine(förslag);
+            }
+
+        }
+    }
+}
