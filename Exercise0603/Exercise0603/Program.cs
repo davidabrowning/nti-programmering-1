c0206fe0b1593656d196a2fc8c0d801fa4477b03
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-22 15:47:44 +0100

    Complete exercise 6.3.

---
+++ Exercise0603/Exercise0603/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise0603
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0;
+            do
+            {
+                Console.WriteLine("Mät vattnet. Hur många grader är det nu?");
+                string str = Console.ReadLine();
+                temp = Convert.ToInt32(str);
+            } while (temp < 100);
+
+            Console.WriteLine("Nu kokar det!");
+        }
+    }
+}
