114ce9965099a0ccab057f6cf1c2661026cf0b9d
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-26 16:15:59 +0100

    Skapa meny och loopstruktur.

--- Ryggsäcken/Ryggsäcken/Program.cs
+++ Ryggsäcken/Ryggsäcken/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,6 +31,46 @@ namespace Ryggsäcken
     {
         static void Main(string[] args)
         {
+            // Deklarera variabler
+            bool avslutaProgrammet = false;
+
+            // Skriva ut en hälsning
+            Console.WriteLine("Välkommen till ryggsäcken!");
+
+            // Så länge användaren inte avslutar programmet
+            while (avslutaProgrammet == false)
+            {
+                // Visa menyval
+                Console.WriteLine("[1] Lägg till ett föremål");
+                Console.WriteLine("[2] Skriv ut innehållet");
+                Console.WriteLine("[3] Rensa innehållet");
+                Console.WriteLine("[4] Avsluta");
+                Console.Write("Välj: ");
+
+                // Användaren gör ett menyval
+                string menyval = Console.ReadLine();
+
+                // Switch menyval
+                switch(menyval)
+                {
+                    case "1":
+                        Console.WriteLine("Lägger till...");
+                        break;
+                    case "2":
+                        Console.WriteLine("Skriver ut...");
+                        break;
+                    case "3":
+                        Console.WriteLine("Renser...");
+                        break;
+                    case "4":
+                        Console.WriteLine("Avslutar programmet. Tack och hej då!");
+                        avslutaProgrammet = true;
+                        break;
+                    default:
+                        Console.WriteLine("Oväntat indata. Visar upp menyn igen.");
+                        break;
+                }
+            }
         }
     }
 }
