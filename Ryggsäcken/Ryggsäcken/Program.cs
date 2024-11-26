d92bd3744c3f460d18d88fce005aaebe1409f4a9
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-26 16:06:27 +0100

    Lägg till programbeskrivning.

--- Ryggsäcken/Ryggsäcken/Program.cs
+++ Ryggsäcken/Ryggsäcken/Program.cs
@@ -4,6 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * PROGRAMBESKRIVNING
+ * SÅ LÄNGE användaren inte avslutar programmet
+ *  Visa menyval
+ *  Användaren gör ett menyval
+ *  SWITCH menyval
+ *      Fall 1:
+ *          Be användaren skriva ett värde
+ *          Spara strängvärde
+ *      Fall 2:
+ *          Skriv ut innehållet
+ *      Fall 3:
+ *          Nollställ
+ *      Fall 4:
+ *          Avsluta programmaet
+ *      Fall default:
+ *          Skriv ut felmeddelande
+ *  SLUT SWITCH
+ * SLUT SÅ LÄNGE
+*/
 namespace Ryggsäcken
 {
     internal class Program
