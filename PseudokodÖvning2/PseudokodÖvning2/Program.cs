cbf11ba1e71a7f1ca0a22d3322655b568fc11726
David <11443294+davidabrowning@users.noreply.github.com>
2024-11-27 10:43:05 +0100

    Skriver program övning 2.

---
+++ PseudokodÖvning2/PseudokodÖvning2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Programbeskrivning:
+ *      Börja räkna en [siffra] från 0
+ *      SÅ LÄNGE [siffra] är under 100
+ *          Addera 1 till [siffra]
+ *      Säg till användaren att programmet har räknat klart
+ */
+
+namespace PseudokodÖvning2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int siffra = 0;
+            while (siffra < 100)
+            {
+                siffra++;
+                Console.WriteLine("Siffra har ökat till " + siffra);
+            }
+            Console.WriteLine("Programmet har räknat klart.");
+        }
+    }
+}
