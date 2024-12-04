3606ffdd8d7861914ff32d2e98a63fcdbf389f2c
David <11443294+davidabrowning@users.noreply.github.com>
2024-12-04 14:04:32 +0100

    Skapa Program.cs. Lägg till programbeskrivning som pseudokod.

---
+++ ÖvningVektorerOchSökning/ÖvningVektorerOchSökning/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Programbeskrivning
+ *  Skapa [minVektor] strängvektor med 5 element
+ *  SÅ LÄNGE användaren inte avslutar programmet
+ *      Presentera meny
+ *      Spara användarens [menyval]
+ *      VÄLJ fall ur [menyval]
+ *          Fall 1:
+ *              FÖR varje element i [minVektor]
+ *                  Tilldela ett strängvärde till elementet
+ *          Fall 2:
+ *              FÖR varje element i [minVektor]
+ *                  Skriv ut elementet
+ *          Fall 3:
+ *              Skapa en falsk bool [minBool]
+ *              Användaren skriver ett sökord
+ *              FÖR varje element  [minVektor]
+ *                  OM sökord är samma som element
+ *                      Skriv ut att sökningen lyckades
+ *                      Sätt [minBool] till sann
+ *              OM [minBool] är falsk
+ *                  Skriv ut felmeddelande
+ *          Fall 4:
+ *              Avsluta programmet
+ * 
+ * Författare: David Browning
+ * Mejl: david.a.browning@gmail.com
+ */
+namespace ÖvningVektorerOchSökning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
