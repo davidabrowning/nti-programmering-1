645583092b8a75d73b96bd09c312b59e05fd73c9
David <11443294+davidabrowning@users.noreply.github.com>
2024-12-04 15:27:47 +0100

    Bygg fall 2: Lägg till föremål.

--- ÖvningVektorerOchSökning/ÖvningVektorerOchSökning/Program.cs
+++ ÖvningVektorerOchSökning/ÖvningVektorerOchSökning/Program.cs
@@ -39,23 +39,30 @@ namespace ÖvningVektorerOchSökning
 
         // Konstanter
         const int RYGGSÄCK_STORLEK = 5;
-        const string MENYVAL_1 = "[1] Lägg till ett föremål";
+        const string MENYVAL_1 = "[1] Lägg till ett föremål (max 5)";
         const string MENYVAL_2 = "[2] Skriv ut alla föremål";
         const string MENYVAL_3 = "[3] Sök i ryggsäcken";
         const string MENYVAL_4 = "[4] Avsluta programmet";
-        const string FELMEDDELANDE_MENYVAL_OGILTIGT_FORMAT = "Ogiltigt menyvalformat. Visar menyn igen.";
+        const string MENYINSTRUKTION_1 = "Föremål att lägga till: ";
+        const string MENYINSTRUKTION_2 = "Ryggsäckens innehål:";
+        const string BEKRÄFTELSE_FÖREMÅL_TILLAGD = "Ny föremål tillagd i ryggsäck: ";
+        const string FELMEDDELANDE_MENYVAL_OGILTIG_FORMAT = "Ogiltigt menyvalformat. Visar menyn igen.";
         const string FELMEDDELANDE_MENYVAL_OGILTIGT_VÄRDE = "Ogiltigt menyvalvärde. Visar menyn igen.";
+        const string FELMEDDELANDE_RYGGSÄCK_FULL = "Ryggsäck är full. Kan inte lägga till föremål.";
+        const string FELMEDDELANDE_FÖREMÅL_OGILTIG_FORMAT = "Ogiltig föremålformat. Visar menyn igen.";
         static void Main(string[] args)
         {
 
             // Deklarera variabler
-            string[] minVektor = new string[RYGGSÄCK_STORLEK];
-            bool körProgrammet = true;
+            string[] minVektor = new string[RYGGSÄCK_STORLEK]; // Håller användarens föremål
+            int minVektorStorlek = 0; // Förvarar ryggsäckens nuvarande storlek
+            bool körProgrammet = true; // Förvarar användarens önskan att köra programmet
 
             // Så länge användaren inte vill avsluta programmet
             while (körProgrammet)
             {
                 // Presentera meny
+                Console.WriteLine();
                 Console.WriteLine(MENYVAL_1);
                 Console.WriteLine(MENYVAL_2);
                 Console.WriteLine(MENYVAL_3);
@@ -64,22 +71,49 @@ namespace ÖvningVektorerOchSökning
                 // Spara användarens menyval
                 if (Int32.TryParse(Console.ReadLine(), out int menyval) == false)
                 {
-                    Console.WriteLine(FELMEDDELANDE_MENYVAL_OGILTIGT_FORMAT);
+                    Console.WriteLine(FELMEDDELANDE_MENYVAL_OGILTIG_FORMAT);
                     continue;
                 }
 
                 // Välj fall ur menyval
                 switch (menyval)
                 {
+
                     case 1:
-                        Console.WriteLine("Valde 1");
+                        if (minVektorStorlek == RYGGSÄCK_STORLEK)
+                        {
+                            Console.WriteLine(FELMEDDELANDE_RYGGSÄCK_FULL);
+                        }
+                        else
+                        {
+                            Console.Write(MENYINSTRUKTION_1);
+                            string nyFöremål = Console.ReadLine().Trim();
+                            if (nyFöremål.Length > 0)
+                            {
+                                minVektor[minVektorStorlek] = nyFöremål;
+                                minVektorStorlek++;
+                                Console.WriteLine(BEKRÄFTELSE_FÖREMÅL_TILLAGD + " " + nyFöremål);
+                            } else
+                            {
+                                Console.WriteLine(FELMEDDELANDE_FÖREMÅL_OGILTIG_FORMAT);
+                            }
+                        }
                         break;
+
                     case 2:
-                        Console.WriteLine("Valde 2");
+                        int föremålNummer = 1;
+                        Console.WriteLine(MENYINSTRUKTION_2);
+                        foreach(string föremål in minVektor)
+                        {
+                            Console.WriteLine(föremålNummer + ". " + föremål);
+                            föremålNummer++;
+                        }
                         break;
+
                     case 3:
                         Console.WriteLine("Valde 3");
                         break;
+
                     case 4:
                         Console.WriteLine("Valde 4. Avslutar programmet.");
                         körProgrammet = false;
