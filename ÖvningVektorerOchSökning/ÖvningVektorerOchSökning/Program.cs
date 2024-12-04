28f0d1abcf5f589da227289961af4949c5cb67e7
David <11443294+davidabrowning@users.noreply.github.com>
2024-12-04 14:20:04 +0100

    Bygg menystruktur.

--- ÖvningVektorerOchSökning/ÖvningVektorerOchSökning/Program.cs
+++ ÖvningVektorerOchSökning/ÖvningVektorerOchSökning/Program.cs
@@ -36,8 +36,60 @@ namespace ÖvningVektorerOchSökning
 {
     internal class Program
     {
+
+        // Konstanter
+        const int RYGGSÄCK_STORLEK = 5;
+        const string MENYVAL_1 = "[1] Lägg till ett föremål";
+        const string MENYVAL_2 = "[2] Skriv ut alla föremål";
+        const string MENYVAL_3 = "[3] Sök i ryggsäcken";
+        const string MENYVAL_4 = "[4] Avsluta programmet";
+        const string FELMEDDELANDE_MENYVAL_OGILTIGT_FORMAT = "Ogiltigt menyvalformat. Visar menyn igen.";
+        const string FELMEDDELANDE_MENYVAL_OGILTIGT_VÄRDE = "Ogiltigt menyvalvärde. Visar menyn igen.";
         static void Main(string[] args)
         {
+
+            // Deklarera variabler
+            string[] minVektor = new string[RYGGSÄCK_STORLEK];
+            bool körProgrammet = true;
+
+            // Så länge användaren inte vill avsluta programmet
+            while (körProgrammet)
+            {
+                // Presentera meny
+                Console.WriteLine(MENYVAL_1);
+                Console.WriteLine(MENYVAL_2);
+                Console.WriteLine(MENYVAL_3);
+                Console.WriteLine(MENYVAL_4);
+
+                // Spara användarens menyval
+                if (Int32.TryParse(Console.ReadLine(), out int menyval) == false)
+                {
+                    Console.WriteLine(FELMEDDELANDE_MENYVAL_OGILTIGT_FORMAT);
+                    continue;
+                }
+
+                // Välj fall ur menyval
+                switch (menyval)
+                {
+                    case 1:
+                        Console.WriteLine("Valde 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Valde 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("Valde 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("Valde 4. Avslutar programmet.");
+                        körProgrammet = false;
+                        break;
+                    default:
+                        Console.WriteLine(FELMEDDELANDE_MENYVAL_OGILTIGT_VÄRDE);
+                        break;
+                }
+            }
+
         }
     }
 }
