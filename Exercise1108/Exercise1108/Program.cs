f5762942d8ae09cda396665d97d3f61bc1c76e09
David <11443294+davidabrowning@users.noreply.github.com>
2024-12-06 11:34:31 +0100

    Bygg struktur på övning 11.08.

---
+++ Exercise1108/Exercise1108/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ================================ NAMESPACE =================================
+// Exercise 11.08. Visar en meny med olika funktioner användaren kan välja
+// mellan.
+// Författare: David Browning, david.a.browning@gmail.com.
+// ============================================================================
+namespace Exercise1108
+{
+
+    // ================================ KLASS =================================
+    // Program. Innehåller Main-metoden.
+    // ========================================================================
+    internal class Program
+    {
+
+        // ============================== METOD ===============================
+        // Main. Start på programmet.
+        // ====================================================================
+        static void Main(string[] args)
+        {
+        }
+
+        // ============================== METOD ===============================
+        // ====================================================================
+    }
+}
