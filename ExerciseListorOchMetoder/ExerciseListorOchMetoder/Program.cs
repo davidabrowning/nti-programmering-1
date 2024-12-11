02a03c54f1e23de07851ff481f2eab0711fcbb08
David <11443294+davidabrowning@users.noreply.github.com>
2024-12-11 15:11:23 +0100

    Dela upp i metoder.

--- ExerciseListorOchMetoder/ExerciseListorOchMetoder/Program.cs
+++ ExerciseListorOchMetoder/ExerciseListorOchMetoder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,101 +9,158 @@ using System.Threading.Tasks;
 // författare David Browning (david.a.browning@gmail.com)
 namespace ExerciseListorOchMetoder
 {
+
+    // ================================ KLASS =================================
+    // Program. Innehåller Main-metoden.
+    // ========================================================================
     class Program
     {
-        // detta är en statisk metod med en int som ett returvärde
-        // metoden tar en parameter i form av ett random objekt av
-        // randomklassen
-        static int RullaTärning(Random slumpObjekt)
+        // Konstanter: Värden
+        const int VANLIG_TÄRNING_MIN = 1;
+        const int VANLIG_TÄRNING_MAX = 6;
+
+        // Konstanter: Meddelanden
+        const string PROGRAM_HÄLSNING = "\n\tVälkommen till tärningsgeneratorn!";
+        const string PROGRAM_HEJ_DÅ = "\n\tTack för att du rullade tärning!";
+        const string MENY_ALTERNATIV = "\n" + 
+            "\t[1] Rulla tärning\n" +
+            "\t[2] Kolla vad du rullade\n" +
+            "\t[3] Avsluta";
+        const string MENY_INSTRUKTION = "\tVälj: ";
+        const string RULLA_HUR_MÅNGA_INSTRUKTION = "\n\tHur många tärningar vill du rulla: ";
+        const string RESULTAT_RUBRIK_TÄRNINGAR = "\n\tRullade tärningar: ";
+        const string RESULTAT_RUBRIK_MEDELVÄRDE = "\n\tMedelvärdet på alla tärningsrull: ";
+        const string VARNING_OGILTIGT_MENYVAL = "\n\tVälj 1-3 från menyn.";
+        const string VARNING_INGA_SPARADE_TÄRNINGSRULL = "\n\tDet finns inga sparade tärningsrull! ";
+
+        // ============================== METOD ===============================
+        // RullaTärning(Random slumpObjekt, int min, int max). Statisk metod
+        // som slumpar fram ett tal mellan min och max. Returnerar det slumpade
+        // int-värdet.
+        // ====================================================================
+        static int RullaTärning(Random slumpObjekt, int min, int max)
         {
-            // här ska du skapa kod som slumpar fram ett tal
-            // mellan 1 och 6, så att metoden "rullar" en 6 sidig
-            // tärning när vi kallar på den
-
-            // metoden ska sedan returnera det rullade värdet
-
-            int min = 1; // minvärde på tärningen
-            int max = 6; // maxvärde på tärningen
-            int tal = 0; // håller resultat när tärningen rullas
-            
-            // räkna ut ett slumpvärde mellan min och max
+            int tal;
             tal = slumpObjekt.Next(min, max + 1);
-
-            // returnera resulterande värde
             return tal;
         }
 
-        static void Main()
+        // ============================== METOD ===============================
+        // RullaTärning(Random slumpObjekt). Slumpar fram ett tal mellan
+        // VANLIG_TÄRNING_MIN (1) och VANLIG_TÄRNING_MAX (6). Returnerar det
+        // slumpade int-värdet.
+        // ====================================================================
+        static int RullaTärning(Random slumpObjekt)
+        {
+            int tal;
+            tal = RullaTärning(slumpObjekt, VANLIG_TÄRNING_MIN, VANLIG_TÄRNING_MAX);
+            return tal;
+        }
+
+        // ============================== METOD ===============================
+        // RullaNågraTärningar. Frågar användaren hur många tärningar ska 
+        // rullas. Rullar tärningar genom att ropa RullaTärning-metoden. Sparar
+        // resultaten i List<int> tärningar. Returnerar ingenting.
+        // ====================================================================
+        static void RullaNågraTärningar(Random slump, List<int> tärningar)
+        {
+            Console.Write(RULLA_HUR_MÅNGA_INSTRUKTION);
+            bool inmatning = int.TryParse(Console.ReadLine(), out int antal);
+            if (inmatning)
+            {
+                for (int i = 0; i < antal; i++)
+                {
+                    tärningar.Add(RullaTärning(slump));
+                }
+            }
+        }
+
+        // ============================== METOD ===============================
+        // KollaRulladeTärningar. Om det finns rullade tärningar, skriver ut
+        // värden på rullade tärningar och räknar ut medelvärdet. Returnerar
+        // ingenting.
+        // ====================================================================
+        static void KollaRulladeTärningar(List<int> tärningar)
+        {
+            int sum = 0; // Innehåller summa av tärningsrullningar
+            int medelvärde = 0; // Innehåller medelvärde av tärningsrullar
+
+            if (tärningar.Count <= 0)
+            {
+                Console.WriteLine(VARNING_INGA_SPARADE_TÄRNINGSRULL);
+            }
+            else
+            {
+                Console.WriteLine(RESULTAT_RUBRIK_TÄRNINGAR);
+                foreach (int tärning in tärningar)
+                {
+                    Console.WriteLine("\t" + tärning); // skriv ut tärning
+                    sum += tärning; // lägg tärning till summa
+                }
+                medelvärde = sum / tärningar.Count; // räkna ut medelvärde
+                Console.WriteLine(RESULTAT_RUBRIK_MEDELVÄRDE + medelvärde); // skriv ut medelvärde
+            }
+        }
+
+        // ============================== METOD ===============================
+        // TaEmotMenyVal. Ber användaren mata in ett menyval. Ropar relevanta
+        // meny-metoden beroende på användarens val. Returnerar falskt ifall
+        // användaren har valt [3] Avsluta, annars returnerar sant.
+        // ====================================================================
+        static bool TaEmotMenyVal(Random slump, List<int> tärningar)
+        {
+            bool kör = true;
+            int val;
+            int.TryParse(Console.ReadLine(), out val);
+
+            switch (val)
+            {
+                case 1:
+                    RullaNågraTärningar(slump, tärningar);
+                    break;
+                case 2:
+                    KollaRulladeTärningar(tärningar);
+                    break;
+                case 3:
+                    kör = false;
+                    break;
+                default:
+                    Console.WriteLine(VARNING_OGILTIGT_MENYVAL);
+                    break;
+            }
+
+            return kör;
+        }
+
+        // ============================== METOD ===============================
+        // VisaMeny. Visar en meny där användaren kan rulla tärning eller
+        // granska resultaten. Returnerar ingenting.
+        // ====================================================================
+        static void VisaMeny()
         {
             Random slump = new Random(); // Skapar en instans av klassen Random för vårt slumptal
             List<int> tärningar = new List<int>(); // listan för att spara våra rullade tärningar
 
-            Console.WriteLine("\n\tVälkommen till tärningsgeneratorn!");
-
             bool kör = true;
             while (kör)
             {
-                Console.WriteLine("\n\t[1] Rulla tärning\n" +
-                    "\t[2] Kolla vad du rullade\n" +
-                    "\t[3] Avsluta");
-                Console.Write("\tVälj: ");
-                int val;
-                int.TryParse(Console.ReadLine(), out val);
-
-                switch (val)
-                {
-                    case 1:
-                        Console.Write("\n\tHur många tärningar vill du rulla: ");
-                        bool inmatning = int.TryParse(Console.ReadLine(), out int antal);
-
-                        if (inmatning)
-                        {
-                            for (int i = 0; i < antal; i++)
-                            {
-                                // här kallar vi på metoden RullaTärning
-                                // och sparar det returnerade värdet i 
-                                // listan tärningar
-                                tärningar.Add(RullaTärning(slump));
-                            }
-                        }
-                        break;
-                    case 2:
-                        int sum = 0; // Skapar en int som ska innehålla medelvärdet av alla tärningsrullningar.
-                        if (tärningar.Count <= 0)
-                        {
-                            Console.WriteLine("\n\tDet finns inga sparade tärningsrull! ");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\n\tRullade tärningar: ");
-                            foreach (int tärning in tärningar)
-                            {
-                                // skriv ut tärning
-                                Console.WriteLine("\t" + tärning);
-
-                                // lägg tärning till summa
-                                sum += tärning;
-                            }
-
-                            // räkna ut medelvärde
-                            // obs!  jag använder inte decimaler, då tärningar ändå bara har hela sidor
-                            int medelvärde = sum / tärningar.Count;
-
-                            // skriv ut medelvärde
-                            Console.WriteLine("\n\tMedelvärdet på alla tärningsrull: " + medelvärde); // Här ska medelvärdet skrivas ut
-                        }
-
-                        break;
-                    case 3:
-                        Console.WriteLine("\n\tTack för att du rullade tärning!");
-                        Thread.Sleep(1000);
-                        kör = false;
-                        break;
-                    default:
-                        Console.WriteLine("\n\tVälj 1-3 från menyn.");
-                        break;
-                }
+                Console.WriteLine(MENY_ALTERNATIV);
+                Console.Write(MENY_INSTRUKTION);
+                kör = TaEmotMenyVal(slump, tärningar);
             }
+        }
+
+        // ============================== METOD ===============================
+        // Main.
+        // Start på programmet. Hälsar användaren, ropar VisaMeny() och säger
+        // hej då till användaren.
+        // ====================================================================
+        static void Main()
+        {
+            Console.WriteLine(PROGRAM_HÄLSNING);
+            VisaMeny();
+            Console.WriteLine(PROGRAM_HEJ_DÅ);
+            Thread.Sleep(1000);
         }
     }
 }
